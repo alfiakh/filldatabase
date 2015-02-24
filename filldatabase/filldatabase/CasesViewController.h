1d89c1079aae3819be27856e52ed01a38080448c
Alfiya <alfiya@alfiambp15.local>
2015-02-24 17:09:51 +0200

    вью для кейсов

---
+++ filldatabase/filldatabase/CasesViewController.h
@@ -0,0 +1,15 @@
+//
+//  CasesViewController.h
+//  filldatabase
+//
+//  Created by Alfiya on 24.02.15.
+//  Copyright (c) 2015 Alfiya Khairetdinova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CasesViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *console;
+
+@end
---
+++ filldatabase/filldatabase/CasesViewController.m
@@ -0,0 +1,52 @@
+//
+//  CasesViewController.m
+//  filldatabase
+//
+//  Created by Alfiya on 24.02.15.
+//  Copyright (c) 2015 Alfiya Khairetdinova. All rights reserved.
+//
+
+#import "CasesViewController.h"
+
+@interface CasesViewController ()
+
+@end
+
+@implementation CasesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)firstCase:(id)sender {
+}
+
+- (IBAction)secondCase:(id)sender {
+}
+
+- (IBAction)thirdCase:(id)sender {
+}
+
+- (IBAction)changeManyNotes:(id)sender {
+}
+
+- (IBAction)deleteManyNotes:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
